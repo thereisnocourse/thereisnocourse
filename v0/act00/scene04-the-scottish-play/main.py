273e7a00fbfa80eb13b1cba2f70278f3bfb11377
Alistair Miles <alimanfoo@googlemail.com>
2024-02-26 22:34:10 +0000

    wip

--- v0/act00/scene04-the-scottish-play/main.py
+++ v0/act00/scene04-the-scottish-play/main.py
@@ -2,6 +2,20 @@ from util import when, get_element_by_id, show, hide
 import code
 
 
+game_state = {
+    "location": "outside",
+    "objects_held": set(),
+    "hall_lit": False,
+    "prophecy_heard": False,
+    "computer_destroyed": False,
+    "forest_defeated": False,
+}
+
+
+class InteractionComplete(SystemExit):
+    pass
+
+
 def play():
     prologue = get_element_by_id("prologue")
     hide(prologue)
@@ -9,7 +23,46 @@ def play():
     screen = get_element_by_id("screen")
     show(screen)
 
-    code.interact(banner="TODO")
+    while not game_state["forest_defeated"]:
+        location = game_state["location"]
+        if location == "outside":
+            play_outside()
+        elif location == "hall":
+            play_hall()
+        elif location == "dungeon":
+            play_dungeon()
+        elif location == "bedroom":
+            play_bedroom()
+        elif location == "battlements":
+            play_battlements()
+        else:
+            raise RuntimeError("unexpected location")
+
+    game_completed()
+
+
+def play_outside():
+    pass
+
+
+def play_hall():
+    pass
+
+
+def play_dungeon():
+    pass
+
+
+def play_bedroom():
+    pass
+
+
+def play_battlements():
+    pass
+
+
+def game_completed():
+    pass
 
 
 @when("click", "#play_button")
